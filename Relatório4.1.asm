5fde6480e3e6d09841eb150df2541d6c4cc1ab3a
Leonardo Pereira <99219066+SaintLeonardo@users.noreply.github.com>
2023-09-25 18:03:21 -0300

    Add files via upload
    Questão 1 do 4º Relatório C208

---
+++ Relatório4.1.asm
@@ -0,0 +1,20 @@
+.data
+
+.text
+
+#1
+li $t1, 10
+li $t2, 25
+li $t3, 43
+li $t4, 89
+
+srl $s1, $t1, 8
+srl $s2, $t2, 8
+srl $s3, $t3, 8
+srl $s4, $t4, 8
+
+#2
+srl $s1, $t1, 4
+srl $s2, $t2, 4
+srl $s3, $t3, 4
+srl $s4, $t4, 4
